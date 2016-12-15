ebfc70438fa12f162adcbcda5a6451c064bb12fd
JuniorBoaventura <junior.boaventura1@gmail.com>
2016-12-16 00:47:40 +0100

    Weather & WeekDays class created

--- supWeather/WeatherType.swift
+++ supWeather/WeatherType.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+import UIKit
+
+enum WeatherType: String {
+    case clearDay   = "clear-day"
+    case clearNight = "clear-night"
+    case rain       = "rain"
+    case snow       = "snow"
+    case sleet      = "sleet"
+    case wind       = "wind"
+    case fog        = "fog"
+    case cloudy     = "cloudy"
+    case partlyCloudyDay = "partly-cloudy-day"
+    case partlyCloudyNight = "partly-cloudy-night"
+    case thunderstorm = "thunderstorm"
+    case tornado    = "tornado"
+    
+    var icon: UIImage? {
+        switch self {
+        case .clearDay:
+            return R.image.icClearDay()
+        case .clearNight:
+            return R.image.icMoon()
+        case .rain:
+            return R.image.icRain()
+        case .snow:
+            return R.image.icSnow()
+        case .wind:
+            return R.image.icWind()
+        case .cloudy, partlyCloudyDay, .partlyCloudyNight:
+            return R.image.icCloudy()
+        case .thunderstorm:
+            return R.image.icThunderstorm()
+        default:
+            return nil
+        }
+    }
+    
+    var summary: String? {
+        switch self {
+        case .clearDay:
+            return "Sunny"
+        case .rain:
+            return "Rain"
+        case .snow:
+            return "Snow"
+        case .wind:
+            return "Windy"
+        case .cloudy, partlyCloudyDay, .partlyCloudyNight:
+            return "Cloudy"
+        case .thunderstorm:
+            return "thunderstorm"
+        default:
+            return nil
+        }
+    }
+}
+
--- supWeather/WeekDays.swift
+++ supWeather/WeekDays.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+enum WeekDays: Int{
+    case Monday = 1
+    case Tuesday = 2
+    case Wednesday = 3
+    case Thursday = 4
+    case Friday = 5
+    case Saturday = 6
+    case Sunday = 7
+    
+    var name: String {
+        switch self {
+        case .Monday:
+            return "Monday"
+        case .Tuesday:
+            return "Tuesday"
+        case .Wednesday:
+            return "Wednesday"
+        case .Thursday:
+            return "Thursday"
+        case .Friday:
+            return "Friday"
+        case .Saturday:
+            return "Saturday"
+        case .Sunday:
+            return "Sunday"
+        }
+    }
+}
